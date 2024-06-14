a1fb10eb30be0db6de6ba4c937b87abe95d87de0
Samuel Nogaret <snogaret@assek.ca>
2024-06-14 16:08:51 -0400

    ci: something

--- changelog.js
+++ changelog.js
@@ -3,15 +3,18 @@ const config = require("conventional-changelog-conventionalcommits");
 
 module.exports = config({
   types: [
-    { type: "feat", section: "New Features" },
-    { type: "fix", section: "Bugs" },
-    { type: "perf", section: "Performance Improvements" },
-    { type: "test", section: "Tests" },
-    { type: "chore", section: "Miscellaneous Chores" },
-    { type: "revert", section: "Reverts" },
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "chore", section: "Chores" },
     { type: "docs", section: "Documentation" },
     { type: "style", section: "Styles" },
-    { type: "ci", section: "CI" },
-    { type: "chore", section: "Chores" },
+    { type: "refactor", section: "Code Refactoring" },
+    { type: "perf", section: "Performance Improvements" },
+    { type: "test", section: "Tests" },
+    { type: "build", section: "Build System" },
+    { type: "ci", section: "Continuous Integration" },
   ],
+  skip: {
+    commit: false,
+  },
 });
