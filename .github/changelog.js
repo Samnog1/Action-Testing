96bd6ddae980e7724117f5a2665e50261148fe95
Samuel Nogaret <snogaret@assek.ca>
2024-06-17 09:25:53 -0400

    refactor: something

---
+++ .github/changelog.js
@@ -0,0 +1,17 @@
+"use strict";
+const config = require("conventional-changelog-conventionalcommits");
+
+module.exports = config({
+  types: [
+    { type: "feat", section: "Features", hidden: false },
+    { type: "fix", section: "Bug Fixes", hidden: false },
+    { type: "chore", section: "Chores", hidden: false },
+    { type: "docs", section: "Documentation", hidden: false },
+    { type: "style", section: "Styles", hidden: false },
+    { type: "refactor", section: "Code Refactoring", hidden: false },
+    { type: "perf", section: "Performance Improvements", hidden: false },
+    { type: "test", section: "Tests", hidden: false },
+    { type: "build", section: "Build System", hidden: false },
+    { type: "ci", section: "Continuous Integration", hidden: false },
+  ],
+});
