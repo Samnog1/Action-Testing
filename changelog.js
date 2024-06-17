6e77135451e837a828be8c6051f0333160982b22
Samuel Nogaret <snogaret@assek.ca>
2024-06-17 09:22:41 -0400

    ci: some very real and fast ci improvments

--- changelog.js
+++ changelog.js
@@ -3,15 +3,15 @@ const config = require("conventional-changelog-conventionalcommits");
 
 module.exports = config({
   types: [
-    { type: "feat", section: "Features" },
-    { type: "fix", section: "Bug Fixes" },
-    { type: "chore", section: "Chores" },
-    { type: "docs", section: "Documentation" },
-    { type: "style", section: "Styles" },
-    { type: "refactor", section: "Code Refactoring" },
-    { type: "perf", section: "Performance Improvements" },
-    { type: "test", section: "Tests" },
-    { type: "build", section: "Build System" },
-    { type: "ci", section: "Continuous Integration" },
+    { type: "feat", section: "Features", hidden: false },
+    { type: "fix", section: "Bug Fixes", hidden: false },
+    { type: "chore", section: "Chores", hidden: false },
+    { type: "docs", section: "Documentation", hidden: false },
+    { type: "style", section: "Styles", hidden: false },
+    { type: "refactor", section: "Code Refactoring", hidden: false },
+    { type: "perf", section: "Performance Improvements", hidden: false },
+    { type: "test", section: "Tests", hidden: false },
+    { type: "build", section: "Build System", hidden: false },
+    { type: "ci", section: "Continuous Integration", hidden: false },
   ],
 });
