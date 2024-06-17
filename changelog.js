4cb37fbf8e0ffa02100a7dd9c751ac5405afe3d4
Samuel Nogaret <snogaret@assek.ca>
2024-06-17 09:50:40 -0400

    docs: some documentation

--- changelog.js
+++ changelog.js
@@ -1,22 +1,17 @@
 "use strict";
+const config = require("conventional-changelog-conventionalcommits");
 
-async function getConfig() {
-  const config = await import("conventional-changelog-conventionalcommits");
-
-  return config.default({
-    types: [
-      { type: "ci", section: "Continuous Integration" },
-      { type: "chore", section: "Miscellaneous Chore" },
-      { type: "docs", section: "Documentation" },
-      { type: "feat", section: "Feature" },
-      { type: "feature", section: "Feature" },
-      { type: "fix", section: "Fixed" },
-      { type: "perf", section: "Performance" },
-      { type: "refactor", section: "Code Refactoring" },
-      { type: "style", section: "Styles" },
-      { type: "test", section: "Tests" },
-    ],
-  });
-}
-
-module.exports = getConfig;
+module.exports = config({
+  types: [
+    { type: "feat", section: "Features" },
+    { type: "fix", section: "Bug Fixes" },
+    { type: "chore", section: "Chores" },
+    { type: "docs", section: "Documentation" },
+    { type: "style", section: "Styles" },
+    { type: "refactor", section: "Code Refactoring" },
+    { type: "perf", section: "Performance Improvements" },
+    { type: "test", section: "Tests" },
+    { type: "build", section: "Build System" },
+    { type: "ci", section: "Continuous Integration" },
+  ],
+});
