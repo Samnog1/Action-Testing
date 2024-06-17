5b144b5c2897787db369e61d63dfff816c11109f
Samuel Nogaret <snogaret@assek.ca>
2024-06-17 10:50:18 -0400

    ci: very real changes

--- changelog.js
+++ changelog.js
@@ -1,16 +1,16 @@
-'use strict'
-const config = require('conventional-changelog-conventionalcommits');
+"use strict";
+const config = require("conventional-changelog-conventionalcommits");
 
 module.exports = config({
-  "types": [
-    { type: 'ci', section: 'Continuous Integration ⚙️' },
-        { type: 'chore', section: 'Chores 🧹' },
-        { type: 'docs', section: 'Documentation 📖' },
-        { type: 'feat', section: 'Feature 🎉' },
-        { type: 'fix', section: 'Bug Fix 🐛' },
-        { type: 'perf', section: 'Performance Improvements ⚡' },
-        { type: 'refactor', section: 'Code Refactoring 🔨' },
-        { type: 'style', section: 'Styles 🎨' },
-        { type: 'test', section: 'Tests ✅',  },
+  types: [
+    { type: "ci", section: "Continuous Integration ⚙️" },
+    { type: "chore", section: "Chores 🧹" },
+    { type: "docs", section: "Documentation 📖" },
+    { type: "feat", section: "Feature 🎉" },
+    { type: "fix", section: "Bug Fix 🐛" },
+    { type: "perf", section: "Performance Improvements ⚡" },
+    { type: "refactor", section: "Code Refactoring 🔨" },
+    { type: "style", section: "Styles 🎨" },
+    { type: "test", section: "Tests ✅" },
   ],
 });
